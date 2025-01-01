2853f1e8991b4aec04745a0ea89e74096a20afa0
Person20020 <kojiino@outlook.com>
2025-01-01 00:11:05 -1000

    removed test log

--- deleteAd.js
+++ deleteAd.js
@@ -2,7 +2,6 @@ var popups = [".banner", "vector-sitenotice-container", ".overlay-banner"]
 
 function removeAds() {
     try {
-        console.log('test');
         var ad = document.querySelectorAll(popups);
         for (var i = 0; i < ad.length; i++) {
             ad[i].remove();
